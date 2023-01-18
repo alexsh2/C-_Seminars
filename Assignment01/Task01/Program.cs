987d119bf46fb230f27cef3b68fed3f8a14b30ad
Aleksandr_Shcherbakov <alexcht@mail.ru>
2023-01-18 17:22:07 +0300

    Updated variable 'inpValue'

--- Assignment01/Task01/Program.cs
+++ Assignment01/Task01/Program.cs
@@ -4,7 +4,7 @@
     a = 2 b = 10 -> max = 10
     a = -9 b = -3 -> max = -3 */
 
-string inpValue;
+string? inpValue;
 int[] numbers = new int[2];
 string[] seq = new string[] { "первое", "второе" };
 
