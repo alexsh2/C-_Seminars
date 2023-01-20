dc33fd7851d545240422eb7fc43f7ed8941a2b45
Aleksandr_Shcherbakov <alexcht@mail.ru>
2023-01-20 23:17:11 +0300

    Add Task03

---
+++ Assignment03/Task03/Program.cs
@@ -0,0 +1,31 @@
+﻿/*  Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+    3 -> 1, 8, 27
+    5 -> 1, 8, 27, 64, 125 */
+
+    Console.Clear();
+
+
+    string inpMessage = "Введите целое число: ";
+
+    GetCubeTable(IntInput(inpMessage));
+
+    // Ввод и конвертация числа
+    int IntInput(string msg)
+    {
+        int num = 0;
+        Console.Write(msg);
+        num = Convert.ToInt32(Console.ReadLine());
+        return num;
+    }
+
+    // Вычисление куба чисел в диапазоне от 1 до N и построение таблицы для вывода в консоль
+    void GetCubeTable(int num)
+    {
+        Console.WriteLine("Число\tКуб числа");
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine("{0}\t{1}", i, Math.Pow(i,3));
+        }
+    }
+
+
