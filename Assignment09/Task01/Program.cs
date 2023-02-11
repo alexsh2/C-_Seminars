d40cd3bec8a7f9ae29bb97795ef3b525c88ae36c
Aleksandr_Shcherbakov <alexcht@mail.ru>
2023-02-11 21:51:55 +0300

    Add Task01

---
+++ Assignment09/Task01/Program.cs
@@ -0,0 +1,47 @@
+﻿/*  Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+    M = 1; N = 5 -> "2, 4"
+    M = 4; N = 8 -> "4, 6, 8" */
+
+Console.Clear();
+
+int numberM = Prompt("Введите число М");
+int numberN = Prompt("Введите число N");
+
+if (numberM >= numberN)
+{
+    Console.WriteLine("Число М должно быть меньше числа N");
+}
+else
+{
+    Console.WriteLine($"Чётные натуральные числа в диапазоне {numberM} - {numberN}:");
+    GetEvenNaturalNumbers(numberM, numberN);
+}
+
+
+
+int Prompt(string message)
+{
+    Console.Write($"{message}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+
+void GetEvenNaturalNumbers(int numberM, int numberN)
+{
+    if (numberM > numberN)
+    {
+        Console.WriteLine();
+        Environment.Exit(0);
+    }
+
+    if (numberM % 2 != 0)
+    {
+        numberM++;
+    }
+
+    Console.Write($"{numberM} ");
+    numberM += 2;
+
+    GetEvenNaturalNumbers(numberM, numberN);
+}
